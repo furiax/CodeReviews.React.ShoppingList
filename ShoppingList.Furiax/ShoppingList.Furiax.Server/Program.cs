f5f12d0e5d6f8ed578b5d7bf7637ef67c7721dd3
furiax <carlmalfliet@proximus.be>
2025-01-24 21:20:16 +0100

    added CORS

--- ShoppingList.Furiax/ShoppingList.Furiax.Server/Program.cs
+++ ShoppingList.Furiax/ShoppingList.Furiax.Server/Program.cs
@@ -2,8 +2,19 @@ using Microsoft.EntityFrameworkCore;
 using ShoppingList.Furiax.Server.Data;
 using ShoppingList.Furiax.Server.Services;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(opt =>
+    {
+        opt.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+        {
+            policy.WithOrigins("https://localhost:58045")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });}
+);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
@@ -11,18 +22,18 @@ builder.Services.AddDbContext<ShoppingListDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IShoppingListService, ShoppingListService>();
 
+
 var app = builder.Build();
 
+app.UseCors(MyAllowSpecificOrigins);
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.MapFallbackToFile("/index.html");
 app.MapControllers();
 
