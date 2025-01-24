7c581b4cf84675d0918be9e20e24160420632c5c
furiax <carlmalfliet@proximus.be>
2025-01-24 14:52:26 +0100

    filled in all service methods

--- ShoppingList.Furiax/ShoppingList.Furiax.Server/Services/IShoppingListService.cs
+++ ShoppingList.Furiax/ShoppingList.Furiax.Server/Services/IShoppingListService.cs
@@ -8,6 +8,6 @@ namespace ShoppingList.Furiax.Server.Services
         public Item? GetItemById(int id);
         public Item AddItem(Item item);
         public Item UpdateItem(Item item);
-        public Item? DeleteItem(int id);
+        public string? DeleteItem(int id);
     }
 }
--- ShoppingList.Furiax/ShoppingList.Furiax.Server/Services/ShoppingListService.cs
+++ ShoppingList.Furiax/ShoppingList.Furiax.Server/Services/ShoppingListService.cs
@@ -5,35 +5,52 @@ namespace ShoppingList.Furiax.Server.Services
 {
     public class ShoppingListService : IShoppingListService
     {
-        private readonly ShoppingListDbContext _context;
+        private readonly ShoppingListDbContext Context;
 
         public ShoppingListService(ShoppingListDbContext context)
         {
-            _context = context;
+            Context = context;
         }
         public Item AddItem(Item item)
         {
-            throw new NotImplementedException();
+            var newItem = Context.Items.Add(item);
+            Context.SaveChanges();
+            return newItem.Entity;
         }
 
-        public Item? DeleteItem(int id)
+        public string? DeleteItem(int id)
         {
-            throw new NotImplementedException();
+            Item itemToDelete = Context.Items.Find(id);
+            if (itemToDelete == null)
+            {
+                return null;
+            }
+            Context.Items.Remove(itemToDelete);
+            Context.SaveChanges();
+            return $"Item with id {id} successfully removed";
         }
 
         public List<Item> GetAllItems()
         {
-            throw new NotImplementedException();
+            return Context.Items.ToList();
         }
 
         public Item? GetItemById(int id)
         {
-            throw new NotImplementedException();
+            Item itemToGet = Context.Items.Find(id);
+            return itemToGet == null ? null : itemToGet;
         }
 
         public Item UpdateItem(Item item)
         {
-            throw new NotImplementedException();
+            Item itemToEdit = Context.Items.Find(item.Id);
+            if (itemToEdit == null)
+            {
+                return null;
+            }
+            Context.Entry(itemToEdit).CurrentValues.SetValues(item);
+            Context.SaveChanges();
+            return itemToEdit;
         }
     }
 }
